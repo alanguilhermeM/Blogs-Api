43c7acf441d1374510ab4691c338f9bf1979b508
alanguilhermeM <alanguilherme13@outlook.com>
2023-08-28 22:02:44 -0300

    Finalizado requisito 11

---
+++ src/models/PostCategory.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define(
+    'PostCategory',
+    {
+      postId: { type: DataTypes.INTEGER, primaryKey: true },
+      categoryId: { type: DataTypes.INTEGER, primaryKey: true },
+    },
+    {
+      timestamps: false,
+      underscored: true,
+      tableName: 'blog_posts',
+    }
+  );
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postId', // se refere ao id de Book na tabela de `users_books`
+      otherKey: 'categoryId', // se refere a outra chave de `users_books`
+    });
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'blogPosts',
+      through: PostCategory,
+      foreignKey: 'categoryId', // se refere ao id de User na tabela de `users_books`
+      otherKey: 'postId',
+    });
+  };
+
+  return PostCategory;
+};
